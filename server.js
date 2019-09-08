6876a7a1185168cc42aca53e2cdbcd5ff05638f5
Sani Ahamed <thesua711@gmail.com>
2019-09-09 01:45:14 +0600

    Till socketIo setup

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+
 app.use(express.static(__dirname));
 //Static __dirname = Current Directory
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({exteded:false}))
 
 var messages = [
     {name: "Sani",message: "Nashita My Lov!"},
@@ -15,7 +24,18 @@ app.get('/messages',(req,res) => {
     res.send(messages);
 })
 
-var server = app.listen(3010,() => {
+app.post('/messages',(req,res)=>{
+    messages.push(req.body);
+    io.emit('message',req.body);
+    res.sendStatus(200);
+})
+
+
+io.on("connection",(socket)=>{
+    console.log('user connected');
+})
+
+var server = http.listen(3010,() => {
     console.log("Server is listening on port",server.address().port)
 
 
