b41c4f929ed60bc37fdebd901608159ce1200201
Sani Ahamed <thesua711@gmail.com>
2019-09-10 03:39:34 +0600

    gg

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var mongoose = require('mongoose');
 
+var dbUrl = 'mongodb+srv://thesua7:arsenal23@cluster0-sutdw.mongodb.net/test?retryWrites=true&w=majority';
 
 app.use(express.static(__dirname));
 //Static __dirname = Current Directory
@@ -14,26 +16,50 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({exteded:false}))
 
+var Message = mongoose.model('Message',{
+    name : String,
+    message : String
+})
+
 var messages = [
     {name: "Sani",message: "Nashita My Lov!"},
     {name: "Sani",message: "I miss seeing u!"} 
 ]
 
 app.get('/messages',(req,res) => {
-
+    
     res.send(messages);
 })
 
 app.post('/messages',(req,res)=>{
-    messages.push(req.body);
-    io.emit('message',req.body);
-    res.sendStatus(200);
+    
+    
+    var message = new Message(req.body)
+    message.save((err =>{
+        if(err){
+            sendStatus(500);
+            console.log("OK!")
+            messages.push(req.body);
+            io.emit('message',req.body);
+            res.sendStatus(200);
+        }
+
+            
+        
+    }))
+
 })
 
 
 io.on("connection",(socket)=>{
     console.log('user connected');
 })
+
+mongoose.connect(dbUrl,(err) =>{
+    console.log('mongoDb connection successful');
+})
+
+
 
 var server = http.listen(3010,() => {
     console.log("Server is listening on port",server.address().port)
